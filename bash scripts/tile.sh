eb75b41491f5d082eda4753e5f204d7b047e3b3a
iamdamosuzuki <morganmorel@bavc.org>
2020-10-09 00:20:58 -0700

    added output scaling to tile.sh and udpated documentation

--- bash scripts/tile.sh
+++ bash scripts/tile.sh
@@ -11,9 +11,14 @@ function tileFilter()
 {
   local width="${1:-4}"    # width - default vaue is 4
   local height="${2:-4}"    # height - default value is 4
+  local outResolution="${3:-none}"  #output resolution. It's "none" if not defined
 
   # Build filter string
-  filter_complex="scale=iw/${width}:ih/${height}:force_original_aspect_ratio=decrease,tile=${width}x${height}:overlap=${width}*${height}-1:init_padding=${width}*${height}-1"
+  if [ "$outResolution" == none ]; then
+    filter_complex="scale=iw/${width}:ih/${height}:force_original_aspect_ratio=decrease,tile=${width}x${height}:overlap=${width}*${height}-1:init_padding=${width}*${height}-1"
+  else
+    filter_complex="scale=iw/${width}:ih/${height}:force_original_aspect_ratio=decrease,tile=${width}x${height}:overlap=${width}*${height}-1:init_padding=${width}*${height}-1,scale=${outResolution/x/:}"
+  fi
 
   # Return full filter string, with necessary prefix and suffix filterchains
   printf '%s%s%s' $filter_complex
@@ -21,7 +26,7 @@ function tileFilter()
 
 # Alter/replace FFmpeg command to desired specification
 
-echo ffmpeg -i "'$1'" -c:v prores -profile:v 3 -filter_complex "$(tileFilter "${@:2}")" "${1}_tile.mov'"
+echo ffmpeg -i "'$1'" -c:v prores -profile:v 3 -filter_complex "$(tileFilter "${@:2}")" "'${1}_tile.mov'"
 
 
 ffmpeg -i "$1" -c:v prores -profile:v 3 -filter_complex "$(tileFilter "${@:2}")" "${1}_tile.mov"
