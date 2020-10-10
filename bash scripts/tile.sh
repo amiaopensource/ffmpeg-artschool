1498a28d14efc9f3d4ba8539bf4c71c2b0d96834
iamdamosuzuki <ultraworld@Ultras-iMac.local>
2020-10-09 21:01:27 -0700

    added regular expression to test for frame size string

--- bash scripts/tile.sh
+++ bash scripts/tile.sh
@@ -6,27 +6,41 @@
 # $1: Input File
 # $2: Tile Columns
 # $3: Tile Rows
+# $4: Output resolution
 
-function tileFilter()
-{
-  local width="${1:-4}"    # width - default vaue is 4
-  local height="${2:-4}"    # height - default value is 4
-  local outResolution="${3:-none}"  #output resolution. It's "none" if not defined
+
+  width="${2:-4}"    # width - default vaue is 4
+  height="${3:-4}"    # height - default value is 4
+  outResolution="${4:-none}"  #output resolution. It's "none" if not defined
 
   # Build filter string
   if [ "$outResolution" == none ]; then
     filter_complex="scale=iw/${width}:ih/${height}:force_original_aspect_ratio=decrease,tile=${width}x${height}:overlap=${width}*${height}-1:init_padding=${width}*${height}-1"
   else
-    filter_complex="scale=iw/${width}:ih/${height}:force_original_aspect_ratio=decrease,tile=${width}x${height}:overlap=${width}*${height}-1:init_padding=${width}*${height}-1,scale=${outResolution/x/:}"
+   if [[ "$outResolution" =~ [0-9]{3}[x][0-9]{3} ]]; then
+      filter_complex="scale=iw/${width}:ih/${height}:force_original_aspect_ratio=decrease,tile=${width}x${height}:overlap=${width}*${height}-1:init_padding=${width}*${height}-1,scale=${outResolution/x/:}"
+   elif [[ "$outResolution" =~ [0-9]{4}[x][0-9]{4} ]]; then
+      filter_complex="scale=iw/${width}:ih/${height}:force_original_aspect_ratio=decrease,tile=${width}x${height}:overlap=${width}*${height}-1:init_padding=${width}*${height}-1,scale=${outResolution/x/:}"
+   elif [[ "$outResolution" =~ [0-9]{2}[x][0-9]{2} ]]; then
+      filter_complex="scale=iw/${width}:ih/${height}:force_original_aspect_ratio=decrease,tile=${width}x${height}:overlap=${width}*${height}-1:init_padding=${width}*${height}-1,scale=${outResolution/x/:}"
+   elif [[ "$outResolution" =~ [0-9]{4}[x][0-9]{3} ]]; then
+      filter_complex="scale=iw/${width}:ih/${height}:force_original_aspect_ratio=decrease,tile=${width}x${height}:overlap=${width}*${height}-1:init_padding=${width}*${height}-1,scale=${outResolution/x/:}"
+   elif [[ "$outResolution" =~ [0-9]{3}[x][0-9]{4} ]]; then
+      filter_complex="scale=iw/${width}:ih/${height}:force_original_aspect_ratio=decrease,tile=${width}x${height}:overlap=${width}*${height}-1:init_padding=${width}*${height}-1,scale=${outResolution/x/:}"
+   elif [[ "$outResolution" =~ [0-9]{3}[x][0-9]{2} ]]; then
+      filter_complex="scale=iw/${width}:ih/${height}:force_original_aspect_ratio=decrease,tile=${width}x${height}:overlap=${width}*${height}-1:init_padding=${width}*${height}-1,scale=${outResolution/x/:}"
+   elif [[ "$outResolution" =~ [0-9]{2}[x][0-9]{3} ]]; then
+      filter_complex="scale=iw/${width}:ih/${height}:force_original_aspect_ratio=decrease,tile=${width}x${height}:overlap=${width}*${height}-1:init_padding=${width}*${height}-1,scale=${outResolution/x/:}"
+   else
+      printf "ERROR: Frame Size must be formated as WIDTHxHEIGHT\n" >&2
+      exit
+   fi
   fi
-
-  # Return full filter string, with necessary prefix and suffix filterchains
-  printf '%s%s%s' $filter_complex
-}
 
 # Alter/replace FFmpeg command to desired specification
 
-echo ffmpeg -i "'$1'" -c:v prores -profile:v 3 -filter_complex "$(tileFilter "${@:2}")" "'${1}_tile.mov'"
+printf "\n\n*******START FFMPEG COMMANDS*******\n" >&2
+echo ffmpeg -i "'$1'" -c:v prores -profile:v 3 -filter_complex $filter_complex "'${1}_tile.mov'"
+printf "********END FFMPEG COMMANDS********\n\n " >&2
 
-
-ffmpeg -i "$1" -c:v prores -profile:v 3 -filter_complex "$(tileFilter "${@:2}")" "${1}_tile.mov"
+ffmpeg -hide_banner -i "$1" -c:v prores -profile:v 3 -filter_complex $filter_complex "${1}_tile.mov"
