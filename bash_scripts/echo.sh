e122125c6b71dcb91c36f2ceafa52b7da445849f
iamdamosuzuki <morganmorel@bavc.org>
2020-10-19 17:14:05 -0700

    used andrews suggestion of normal blend mode with lowered opacity to get some great effects

--- bash_scripts/echo.sh
+++ bash_scripts/echo.sh
@@ -5,25 +5,25 @@
 # Parameters:
 # $1: File 1 (the overaly file, or the file that will be keyed)
 # $2: Echo rate
-# $3: Number of trails
+# $3: Length of tails. 1 is just one tail, number increases exponentially as it goes. 
 
-echoRate="${2:-0.1}"          # Echo rate
-trails="${3:-1}"    # Number of trails
+echoRate="${2:-0.2}"          # Echo rate
+trails="${3:-3}"    # Number of trails
 echofeedback=""
-testvariable="1234"
+ptsDelay="0"
 
 for (( i=1;i<${trails};i++ ))
 {
    # 'bc' command used for floating point addition
-   echofeedback="[wet]split[wet2Blend][wet2Feedback],[wet2Feedback]setpts=PTS+$echoRate/TB[wetFromFeedback];[wet2Blend]lutyuv=y=val*0.75[wet2Blend];[wetFromFeedback]lutyuv=y=val*0.50[wetFromFeedback];[wetFromFeedback][wet2Blend]blend=all_mode=screen[wet];"$echofeedback
+   ptsDelay="$(bc <<<"${ptsDelay}+${echoRate}")"
+   echofeedback="[wet]split[wet2Blend][wet2Feedback],[wet2Feedback]setpts=PTS+($ptsDelay/TB)[wetFromFeedback];[wetFromFeedback][wet2Blend]blend=all_mode=normal:c0_opacity=.5:c1_opacity=.5[wet];"$echofeedback
 }
 
-echofilter="format=rgb24,split[dry][toEcho];[toEcho]setpts=PTS+$echoRate/TB[wet];$echofeedback[dry]lutyuv=y=val*0.75[dry];[wet]lutyuv=y=val*0.50[wet];[wet][dry]blend=all_mode=screen,format=yuv422p10le[out]"
+echofilter="split[dry][toEcho];[toEcho]setpts=PTS+($echoRate/TB)[wet];$echofeedback[wet][dry]blend=all_mode=normal:c0_opacity=.5:c1_opacity=.5[outRGB];[outRGB]format=yuv422p10le[out]"
 
 # Alter/replace FFmpeg command to desired specification
 printf "\n\n*******START FFMPEG COMMANDS*******\n" >&2
 echo ffmpeg -i "'$1'" -c:v prores -profile:v 3 -filter_complex $echofilter -map '[out]' "'${1%%.*}_echo.mov'"
 printf "********END FFMPEG COMMANDS********\n\n " >&2
-echo $echofeedback
 
 ffmpeg -i "$1" -c:v prores -profile:v 3 -filter_complex $echofilter -map '[out]' "${1%%.*}_echo.mov"
