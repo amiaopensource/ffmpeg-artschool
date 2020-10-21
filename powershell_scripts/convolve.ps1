9b72bef54f3b6547e6c2e2b83c37829a0683ef48
Nick Krabbenhoeft <nickkrabbenhoeft@nypl.org>
2020-10-20 22:45:03 -0400

    Update convolve.ps1

--- powershell_scripts/convolve.ps1
+++ powershell_scripts/convolve.ps1
@@ -25,17 +25,5 @@ Param(
     [System.IO.FileInfo]$v2
 )
 
-# Convert key color to hex code
-Switch ($key)
-{
-    "blue" {$hexkey = "0000FF"}
-    "green" {$hexkey = "00FF00"}
-    "red" {$hexkey = "FF0000"}
-    "purple" {$hexkey = "FF00FF"}
-    "orange" {$hexkey = "FF9900"}
-    "yellow" {$hexkey = "FFFF00"}
-}
-
-
 # Run ffmpeg command
 ffmpeg.exe -i $v1 -i $v2 -c:v prores_ks -profile:v 3 -filter_complex '[0:v][1:v]scale2ref[v0][v1];[v0]format=yuv422p10le[v0];[v1]format=yuv422p10le[v1];[v0][v1]convolve [out]' -map '[out]' "$((Get-Item $v1).Basename)_convolve.mov"
