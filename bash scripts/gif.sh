c71aa175d35e92818379dce54fd5d48366d80932
bturkus <benjaminturkus@nypl.org>
2020-10-22 11:19:09 -0400

    add gif and trim scripts

---
+++ bash scripts/gif.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Turn an input video file into a gif
+
+# Parameters:
+# $1: Input File
+
+palette=$(dirname "$1")/palette.png
+
+printf "\n\n*******START FFMPEG COMMANDS*******\n" >&2
+echo ffmpeg -i "$1" -filter_complex "fps=10,scale=500:-1:flags=lanczos,palettegen" $palette
+echo ffmpeg -i "$1" -i $palette -filter_complex "[0:v]fps=10, scale=500:-1:flags=lanczos[v], [v][1:v]paletteuse" "${1%%.*}.gif"
+printf "********END FFMPEG COMMANDS********\n\n " >&2
+
+ffmpeg -i "$1" -filter_complex "fps=10,scale=500:-1:flags=lanczos,palettegen" $palette
+ffmpeg -i "$1" -i $palette -filter_complex "[0:v]fps=10, scale=500:-1:flags=lanczos[v], [v][1:v]paletteuse" "${1%%.*}.gif"
+rm $palette
