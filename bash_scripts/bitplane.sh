1a58afca67bbf17ff4c294254ef576e2a3a93d5c
iamdamosuzuki <ultraworld@Ultras-iMac.local>
2020-10-29 12:40:00 -0700

    normalized to proper style with getopts

--- bash_scripts/bitplane.sh
+++ bash_scripts/bitplane.sh
@@ -2,55 +2,65 @@
 
 # Manipulate Y, U, and V bitplanes to make colorful creations
 
+# Parameters:
+# $1: Input File
+# $2: Y bitplane
+# $3: U bitplane
+# $4: V bitplane
+
 _usage(){
 cat <<EOF
 $(basename "${0}")
   Usage
-   $(basename "${0}") [OPTIONS] MOVIE_FILE_1.mov Y value, U value, V, value
+   $(basename "${0}") [OPTIONS] INPUT_FILE Y_BITPLANE U_BITPLANE V_BITPLANE
   Options
    -h  display this help
    -p  previews in FFplay
    -s  saves to file with FFmpeg
+
+  Notes
+
+
   Outcome
-   will play or save file with user-selected or random bitplane values
+   Manipulate Y, U, and V bitplanes to make colorful creations
    dependencies: ffmpeg 4.3 or later
 EOF
 }
 
-low=1
-high=8
-rand1=$((low + RANDOM%(1+high-low)))
-rand2=$((low + RANDOM%(1+high-low)))
-rand3=$((low + RANDOM%(1+high-low)))
 
-Y="${3:-$rand1}"    # user selected or, if not, random default value bet 1-8
-U="${4:-$rand2}"    # user selected or, if not, random default value bet 1-8
-V="${5:-$rand3}"    # user selected or, if not, random default value bet 1-8
+function filter_complex()
+{
 
-vf=$vf"format=yuv420p10le|yuv422p10le|yuv444p10le|yuv440p10le,lutyuv=y=if(eq($Y\,-1)\,512\,if(eq($Y\,0)\,val\,bitand(val\,pow(2\,10-$Y))*pow(2\,$Y))):u=if(eq($U\,-1)\,512\,if(eq($U\,0)\,val\,bitand(val\,pow(2\,10-$U))*pow(2\,$U))):v=if(eq($V\,-1)\,512\,if(eq($V\,0)\,val\,bitand(val\,pow(2\,10-$V))*pow(2\,$V))),format=yuv422p10le"
+   low=1
+   high=8
+   rand1=$((low + RANDOM%(1+high-low)))
+   rand2=$((low + RANDOM%(1+high-low)))
+   rand3=$((low + RANDOM%(1+high-low)))
 
-while getopts "hps" OPT ; do
+   Y="${3:-$rand1}"    # user selected or, if not, random default value bet 1-8
+   U="${4:-$rand2}"    # user selected or, if not, random default value bet 1-8
+   V="${5:-$rand3}"    # user selected or, if not, random default value bet 1-8
+
+   vf=$vf"format=yuv420p10le|yuv422p10le|yuv444p10le|yuv440p10le,lutyuv=y=if(eq($Y\,-1)\,512\,if(eq($Y\,0)\,val\,bitand(val\,pow(2\,10-$Y))*pow(2\,$Y))):u=if(eq($U\,-1)\,512\,if(eq($U\,0)\,val\,bitand(val\,pow(2\,10-$U))*pow(2\,$U))):v=if(eq($V\,-1)\,512\,if(eq($V\,0)\,val\,bitand(val\,pow(2\,10-$V))*pow(2\,$V))),format=yuv422p10le"
+
+   # Return full filter string, with necessary prefix and suffix filterchains
+   printf '%s%s%s' $vf
+
+}
+
+while getopts "ps" OPT ; do
     case "${OPT}" in
-      h) _usage ; exit 0
-        ;;
       p)
         printf "\n\n*******START FFPLAY COMMANDS*******\n" >&2
-        echo "Y=$Y"
-        echo "U=$U"
-        echo "V=$V"
-        echo ffplay "$2" -vf $vf
+        printf "echo ffplay '$2' -vf $(filter_complex)" >&2
         printf "********END FFPLAY COMMANDS********\n\n " >&2
-        ffplay "$2" -vf $vf
+        ffplay "$2" -vf $(filter_complex)
         ;;
       s)
         printf "\n\n*******START FFMPEG COMMANDS*******\n" >&2
-        echo "Y=$Y"
-        echo "U=$U"
-        echo "V=$V"
-        echo ffmpeg -i "'$2'" -c:v prores -profile:v 3 -filter_complex $vf "'${2}_bitplane.mov'"
+        echo ffmpeg -i "'$2'"  -c:v prores -profile:v 3 -filter_complex $(filter_complex) "'${2%%.*}_bitplane.mov'"
         printf "********END FFMPEG COMMANDS********\n\n " >&2
-        ffmpeg -hide_banner -i "$2" -c:v prores -profile:v 3 -vf $vf "${2}_bitplane.mov"
+        ffmpeg -hide_banner -i "$2"  -c:v prores -profile:v 3 -vf $(filter_complex) "${2%%.*}_bitplane.mov"
         ;;
-      *) echo "bad option -${OPTARG}" ; _usage ; exit 1 ;
     esac
   done
--- bash_scripts/chromakey.sh
+++ bash_scripts/chromakey.sh
@@ -68,15 +68,15 @@ while getopts "hps" OPT ; do
         ;;
       p)
       printf "\n\n*******START FFPLAY COMMANDS*******\n" >&2
-      echo ffmpeg -i "$3" -i "$2" -c:v prores -filter_complex "$(chromakeyFilter "${@:3}")" -map '[v]' -f matroska - | ffplay -
+      printf "ffmpeg -i '$3' -i '$2' -c:v prores -filter_complex $(chromakeyFilter ${@:4}) -map '[v]' -f matroska - | ffplay - \n" >&2
       printf "********END FFPLAY COMMANDS********\n\n " >&2
       ffmpeg -i "$3" -i "$2" -c:v prores -filter_complex "$(chromakeyFilter "${@:3}")" -map '[v]' -f matroska - | ffplay -
         ;;
       s)
         printf "\n\n*******START FFMPEG COMMANDS*******\n" >&2
-        echo ffmpeg -i "$3" -i "$2" -c:v prores -profile:v 3 -filter_complex "$(chromakeyFilter "${@:3}")" -map '[v]' "${2%%.*}_chromakey.mov"
+        printf "ffmpeg -i '$3' -i '$2' -c:v prores -filter_complex $(chromakeyFilter ${@:4}) -map '[v]' -f matroska - | ffplay - \n" >&2
         printf "********END FFMPEG COMMANDS********\n\n " >&2
-        ffmpeg -hide_banner -i "$3" -i "$2" -c:v prores -profile:v 3 -filter_complex "$(chromakeyFilter "${@:3}")" -map '[v]' "${2%%.*}_chromakey.mov"
+        ffmpeg -hide_banner -i "$3" -i "$2" -c:v prores -profile:v 3 -filter_complex "$(chromakeyFilter "${@:4}")" -map '[v]' "${2%%.*}_chromakey.mov"
         ;;
       *) echo "bad option -${OPTARG}" ; _usage ; exit 1 ;
     esac
