2f3011dc20a632fd138f063762399f9ed22d09b8
bturkus <benjaminturkus@nypl.org>
2020-11-01 13:56:37 -0500

    Update blend.sh
    - re-ordered print statements
    - fixed fix suffix
    - added some quotes to ensure working print statements

--- bash_scripts/blend.sh
+++ bash_scripts/blend.sh
@@ -53,16 +53,16 @@ while getopts "hps" OPT ; do
       h) _usage ; exit 0
         ;;
       p)
-      printf "\n\n*******START FFPLAY COMMANDS*******\n" >&2
-      printf "ffmpeg -hide_banner -stats -i '$3' -i '$2' -c:v prores -filter_complex $(filter_complex ${@:4}) -map '[v]' -f matroska - | ffplay - \n" >&2
-      printf "********END FFPLAY COMMANDS********\n\n " >&2
       ffmpeg -hide_banner -i "$3" -i "$2" -c:v prores -filter_complex "$(filter_complex "${@:4}")" -map '[v]' -f matroska - | ffplay -
+      printf "\n\n*******START FFPLAY COMMANDS*******\n" >&2
+      printf "ffmpeg -hide_banner -stats -i '$3' -i '$2' -c:v prores -filter_complex '$(filter_complex ${@:4})' -map '[v]' -f matroska - | ffplay - \n" >&2
+      printf "********END FFPLAY COMMANDS********\n\n " >&2
         ;;
       s)
+        ffmpeg -hide_banner -i "$3" -i "$2" -c:v prores -profile:v 3 -filter_complex "$(filter_complex "${@:4}")" -map '[v]' "${2%%.*}_blend.mov"
         printf "\n\n*******START FFMPEG COMMANDS*******\n" >&2
-        printf "ffmpeg -hide_banner -i '$3' -i '$2' -c:v prores -filter_complex $(filter_complex ${@:4}) -map '[v]' -f matroska - | ffplay - \n" >&2
+        printf "ffmpeg -hide_banner -i '$3' -i '$2' -c:v prores -filter_complex '$(filter_complex ${@:4})' -map '[v]' ${2%%.*}_blend.mov \n" >&2
         printf "********END FFMPEG COMMANDS********\n\n " >&2
-        ffmpeg -hide_banner -i "$3" -i "$2" -c:v prores -profile:v 3 -filter_complex "$(filter_complex "${@:4}")" -map '[v]' "${2%%.*}_chromakey.mov"
         ;;
       *) echo "bad option -${OPTARG}" ; _usage ; exit 1 ;
     esac
