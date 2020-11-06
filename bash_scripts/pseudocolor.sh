3905e1ac1ed6aaa6c2bf5544c0f3b2892f1d4185
iamdamosuzuki <ultraworld@Ultras-iMac.local>
2020-11-06 10:29:59 -0800

    filter string had disappeared. fix this

--- bash_scripts/pseudocolor.sh
+++ bash_scripts/pseudocolor.sh
@@ -25,6 +25,7 @@ $(basename "${0}")
 EOF
 }
 
+filter_string="eq=brightness=0.40:saturation=8,pseudocolor='if(between(val,ymax,amax),lerp(ymin,ymax,(val-ymax)/(amax-ymax)),-1):if(between(val,ymax,amax),lerp(umax,umin,(val-ymax)/(amax-ymax)),-1):if(between(val,ymax,amax),lerp(vmin,vmax,(val-ymax)/(amax-ymax)),-1):-1'"
 
 while getopts "hps" OPT ; do
     case "${OPT}" in
