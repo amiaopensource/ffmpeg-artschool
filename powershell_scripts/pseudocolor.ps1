291fb87e121710ee2958bc729ccf1379777d24c1
nkrabben <nkrabben@gmail.com>
2020-11-09 14:00:55 -0500

    add pseudo, prob works

---
+++ powershell_scripts/pseudocolor.ps1
@@ -0,0 +1,95 @@
+<#
+.DESCRIPTION
+    Boosts levels and then adds gradient for out of range pixels using pseudocolor
+.PARAMETER h
+    display this help
+.PARAMETER p
+    previews in FFplay
+.PARAMETER s
+    saves to file with FFmpeg
+.PARAMETER video
+    path to the video
+#>
+
+
+# Parse arguments
+
+Param(
+    [Parameter(ParameterSetName="Help")]
+    [Parameter(ParameterSetName="Run")]
+    [Switch]
+    $h,
+
+    [Parameter(ParameterSetName="Run")]
+    [Switch]
+    $p,
+
+    [Parameter(ParameterSetName="Run")]
+    [Switch]
+    $s = $true,
+
+    [Parameter(Position=0, Mandatory, ParameterSetName="Run")]
+    [ValidateScript({
+        if(-Not ($_ | Test-Path) ){
+            throw "File or folder does not exist" 
+        }
+        if(-Not ($_ | Test-Path -PathType Leaf) ){
+            throw "The Path argument must be a file. Folder paths are not allowed."
+        }
+        return $true
+    })]
+    [System.IO.FileInfo]$video
+)
+
+
+# Display help
+
+if (($h) -or ($PSBoundParameters.Values.Count -eq 0 -and $args.count -eq 0)){
+    Get-Help $MyInvocation.MyCommand.Definition -detailed
+    if (!$video) {
+        exit
+    }
+}
+
+
+# Create filter string
+
+$filter_string="eq=brightness=0.40:saturation=8,pseudocolor='if(between(val,ymax,amax),lerp(ymin,ymax,(val-ymax)/(amax-ymax)),-1):if(between(val,ymax,amax),lerp(umax,umin,(val-ymax)/(amax-ymax)),-1):if(between(val,ymax,amax),lerp(vmin,vmax,(val-ymax)/(amax-ymax)),-1):-1'"
+
+
+# Run command
+
+if ($p) {
+    $tempFile = New-TemporaryFile
+    ffmpeg.exe -hide_banner -stats -y -i $video -c:v prores -profile:v 3 -filter_complex $filter -f matroska $tempFile
+    ffplay.exe $tempFile
+    
+Write-Host @"
+
+
+*******START FFPLAY COMMANDS*******
+
+ffmpeg.exe -hide_banner -stats -y -i $video -c:v prores -profile:v 3 -filter_complex $($filter) -f matroska -f matroska $tempFile
+ffplay $tempFile.FullName
+
+********END FFPLAY COMMANDS********
+
+
+"@
+}
+else {
+    ffmpeg.exe -hide_banner -i $video -c:v prores -profile:v 3 $filter "$((Get-Item $video).Basename)_pseudocolor.mov"
+
+Write-Host @"
+
+
+*******START FFMPEG COMMANDS*******
+
+ffmpeg.exe -hide_banner -i $video -c:v prores -profile:v 3 $($filter) `"$((Get-Item $video).Basename)_pseudocolor.mov`"
+
+********END FFMPEG COMMANDS********
+
+
+"@
+}
+
