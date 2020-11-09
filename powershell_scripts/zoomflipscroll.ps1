29dea78063d7d319e0bdc3f9dd37361bea43aeb8
nkrabben <nkrabben@gmail.com>
2020-11-09 16:13:01 -0500

    add zfs, but more of a copy on write

---
+++ powershell_scripts/zoomflipscroll.ps1
@@ -0,0 +1,100 @@
+<#
+.DESCRIPTION
+    zoom in on a single line of video, flip and scroll vertically
+.PARAMETER h
+    display this help
+.PARAMETER p
+    previews in FFplay
+.PARAMETER s
+    saves to file with FFmpeg
+.PARAMETER video
+    path to the video
+.PARAMETER line
+    line to zoom [default: random]
+#>
+
+
+# Parse arguments
+Param(
+    [Parameter(ParameterSetName="Help")]
+    [Parameter(ParameterSetName="Run")]
+    [Switch]
+    $h,
+
+    [Parameter(ParameterSetName="Run")]
+    [Switch]
+    $p,
+
+    [Parameter(ParameterSetName="Run")]
+    [Switch]
+    $s = $true,
+
+    [Parameter(Position=0, Mandatory, ParameterSetName="Run")]
+    [ValidateScript({
+        if(-Not ($_ | Test-Path) ){
+            throw "File or folder does not exist" 
+        }
+        if(-Not ($_ | Test-Path -PathType Leaf) ){
+            throw "The Path argument must be a file. Folder paths are not allowed."
+        }
+        return $true
+    })]
+    [System.IO.FileInfo]$video,
+
+    [Parameter(Position=1, Mandatory, ParameterSetName="Run")]
+    [ValidateRange(0, 1280)]
+    $line = $(Get-Random -Minimum -1 -Maximum 350)
+)
+
+
+# Display help
+
+if (($h) -or ($PSBoundParameters.Values.Count -eq 0 -and $args.count -eq 0)){
+    Get-Help $MyInvocation.MyCommand.Definition -detailed
+    if (!$video) {
+        exit
+    }
+}
+
+
+# Create filter string
+
+$filter = "format=rgb24,crop=iw:1:0:$line,scale=iw:4:flags=neighbor,tile=layout=1x120:overlap=119:init_padding=119,setdar=4/3,format=yuv422p10le"
+
+
+# Run command
+
+if ($p) {
+    $tempFile = New-TemporaryFile
+    ffmpeg.exe -hide_banner -stats -i $video -c:v prores -profile:v 3 -filter_complex $filter -map "[v]" -f matroska $tempFile
+    ffplay.exe $tempFile
+    
+    Write-Host @"
+
+
+*******START FFPLAY COMMANDS*******
+
+ffmpeg.exe -hide_banner -stats -y -i $video -c:v prores -profile:v 3 -filter_complex $filter -map "[v]" -f matroska $tempFile
+ffplay $tempFile.FullName
+
+********END FFPLAY COMMANDS********
+
+
+"@
+}
+else {
+    ffmpeg.exe -hide_banner -i $video -c:v prores -profile:v 3 -filter_complex $filter -map "[v]" "$((Get-Item $video1).Basename)_zoomscroll.mov"
+
+    Write-Host @"
+
+
+*******START FFMPEG COMMANDS*******
+
+ffmpeg.exe -hide_banner -i $video -c:v prores -profile:v 3 -filter_complex $filter -map "[v]" "$((Get-Item $video1).Basename)_zoomscroll.mov"
+
+********END FFMPEG COMMANDS********
+
+
+"@
+}
+
